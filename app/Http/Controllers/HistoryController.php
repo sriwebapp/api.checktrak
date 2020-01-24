60cb065a769342ad10b2aa8f35292bda80ed1bc9
Arnel Forbes <forbesarnel09@gmail.com>
2020-01-24 09:50:26 +0800

    history date updating

---
+++ app/Http/Controllers/HistoryController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Company;
+use App\History;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class HistoryController extends Controller
+{
+    public function index()
+    {
+        abort(404);
+    }
+
+    public function store(Request $request)
+    {
+        abort(403);
+    }
+
+    public function show(History $history)
+    {
+        abort(404);
+    }
+
+    public function update(Request $request, Company $company, History $history)
+    {
+        // authorization
+        $this->authorize('undo', [$history->check, $company]);
+
+        request()->validate([ 'date' => 'required|date' ]);
+
+        $date = new Carbon(request('date'));
+
+        $history->update(['date' => $date->format('Y-m-d')]);
+
+        Log::info(request()->user()->name . ' updated history date.');
+
+        return ['message' => 'History successfully updated.'];
+    }
+
+    public function destroy(History $history)
+    {
+        abort(403);
+    }
+}
